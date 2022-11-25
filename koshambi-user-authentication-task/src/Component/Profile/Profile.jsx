fad14b1be19fe63f71e5fd0b1c0004fd4194de9a
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-11-25 23:44:31 +0600

    Added Signout & Profile

--- koshambi-user-authentication-task/src/Component/Profile/Profile.jsx
+++ koshambi-user-authentication-task/src/Component/Profile/Profile.jsx
@@ -1,9 +1,24 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 
 const Profile = () => {
+    const [user] = useAuthState(auth);
+    const { displayName, email } = user;
+    const handleLogOut = () =>{
+        signOut(auth);
+    }
     return (
-        <div className='ml-[300px]'>
-            <h2>Profile</h2>
+        <div className='container mx-auto flex justify-center items-center'>
+            <div className="profile__content p-10 lg:p-0">
+                <h2 className='text-2xl lg:text-5xl font-bold'>Profile</h2>
+                <h3 className='capitalize text-xl lg:text-3xl font-semibold my-3'>Name: {displayName}</h3>
+                <p className='text-xl lg:text-3xl font-semibold'>Your Email: {email}</p>
+                <div className="logout__btn mt-6">
+                    <button className='border-2 border-black text-black font-semibold hover:text-white hover:bg-black px-6 lg:px-8 py-2' onClick={handleLogOut}>Logout</button>
+                </div>
+            </div>
         </div>
     );
 };
